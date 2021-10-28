38e13b644cfb9b58a015cad8edb8a4b969ea3b5a
OneOrp <oneorp@gmail.com>
2021-10-28 20:27:55 +0300

    Create Lesson10-HW.py

---
+++ Lesson10-HW.py
@@ -0,0 +1,41 @@
+'''
+1. Function is a block of code that runs only when it is called.
+2. def function_name():
+    a. def function_name(x=3, y=10):
+        b. The reason to add deafult paramentrs is to 
+            c. 
+4.
+5.
+6.
+7.
+    a. with random.randint
+        b. random
+            c. import random
+8.  def getCircleHekef(r):
+9.         _hekef = 2 * math.pi * r
+10.         return _hekef
+11.     
+12.     getCircleHekef()
+13.     
+14. 9.
+15. 10.
+16. 11. def minimum():
+17.         lst = []
+18.         for i in range(1, 3 + 1, 1):
+19.             x = int(input('Enter 3 numbers: '))
+20.             lst.append(x)
+21.         print(f'lowest number is {min(lst)}')
+    minimum()
+
+
+
+def get_in_range(min, max):
+    while True:
+        number = int(input(f'enter number in range({_min}-{_max}')))
+        return number
+
+
+
+
+
+'''
